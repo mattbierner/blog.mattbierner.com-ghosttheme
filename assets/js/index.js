028f06f3a50e46224e106e8900707bfecd47597f
Matt Bierner <mattbierner@gmail.com>
2014-05-02 14:04:12 -0400

    Reduce bold headers, added figcaption to imgs.

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,15 @@
         // for better relative/absolute positioning.
 
         //$("#blog-logo").prependTo("#site-head-content");
-
+        
+        // Add caption to imgs
+        // https://ghost.org/forum/using-ghost/2797-image-captions/
+        $(".post-content img").each(function() {
+            if ($(this).attr("alt"))
+              $(this)
+                  .wrap('<figure class="image"></figure>')
+                  .after('<figcaption>' + $(this).attr("alt") + '</figcaption>');
+        });
     });
 
 }(jQuery));
