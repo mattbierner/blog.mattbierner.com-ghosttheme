677e9f91ac16823946f045c8f49152536ee73d4a
Matt Bierner <mattbierner@gmail.com>
2014-12-07 14:05:28 -0800

    Added page links to post

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,6 +21,81 @@
                   .wrap('<figure class="image"></figure>')
                   .after('<figcaption>' + $(this).attr("alt") + '</figcaption>');
         });
+        
+        SetupPagination();
     });
 
 }(jQuery));
+
+
+/**
+ * Create pagination links
+ * 
+ * From: https://gist.github.com/jyek/5141bc6166b01419d43f
+ * 
+ * Fuck Ghost, making us have to parse RSS client side.
+ */
+var SetupPagination = function() {
+    var NextPrevLinksModule = function(){
+    var curr,
+        $prevLink,
+        $nextLink;
+ 
+    return {
+      init: function(){
+        curr = $('#curr-post-uuid').html();
+        $prevLink = $('.prev-post');
+        $nextLink = $('.next-post');
+ 
+        $prevLink.hide();
+        $nextLink.hide();
+ 
+        this.parseRss();
+      },
+      // creates previous and next links
+      createLinks: function(items){
+        for (var i = 0; i < items.length; i++){
+          var uuid = $(items[i]).find('guid').text(); console.log((items[i+1]))
+          if (uuid === curr){
+            if (i < items.length-1){
+              $prevLink
+                .attr('href', $(items[i+1]).find('link').text())
+                .attr('title', $(items[i+1]).find('title').text());
+              $prevLink.show(); 
+            }
+            if (i > 0){
+              $nextLink
+                .attr('href', $(items[i-1]).find('link').text())
+                .attr('title', $(items[i-1]).find('title').text());;
+              $nextLink.show();
+            }
+          }
+        }
+      },
+      // iteratively parses rss feeds to generate posts object
+      parseRss: function(page, items, lastId){
+        self = this;
+        page = page || 1;
+        items = items || undefined;
+        lastId = lastId || '';
+        $.get('/rss/' + page, function(data){
+          var posts = $(data).find('item');
+          var currId;
+          if (posts.length > 0) currId = $(posts[0]).find('guid').text();
+ 
+          if (currId === lastId){
+            // if this page has already been parsed, create links
+            self.createLinks(items);
+          } else {
+            // if not, continue parsing posts
+            items = items ? items.add(posts) : posts;
+            lastId = currId;
+            self.parseRss(page+1, items, lastId);
+          }
+        });
+      }
+    };
+  }();
+ 
+  NextPrevLinksModule.init();
+};
